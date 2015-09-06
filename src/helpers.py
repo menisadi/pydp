4d0763d22839aebcb957fe5896535ca3acb8a12f
menisadi <menitm@gmail.com>
2015-09-06 22:13:17 +0300

    expo

--- src/helpers.py
+++ src/helpers.py
@@ -5,5 +5,11 @@ def noisyMax(X,q,eps) :
 	noisy = [q(i)+np.random.laplace(0, 1/eps, 1) for i in X]
 	return 	noisy.index(max(noisy))
 
-def exponential(X,q,eps) :
-	norm += (math.exp(eps*q(X,r))
+def expo(X,q,eps) :
+	X_chances = [math.exp(eps*q(X,r)) for r in X]
+	normalizator = sum(X_chances)
+	X_chances = [i/normalizator for i in X_chances]
+	X_CDF = np.cumsum(X_chances).tolist()
+	pick = np.random.rand()
+	return np.searchsorted(X_CDF,pick) + 1
+
