33c78560b8b19cb8ad0eeea77733566beb6dce3e
kobbi nissim <kobbini@hotmail.com>
2015-09-10 14:51:09 -0400

    small edits

--- src/basicdp.py
+++ src/basicdp.py
@@ -1,26 +1,27 @@
 import numpy as np
 import math
 
-def noisy_max(X,R,q,eps) :
+def noisy_max(X,D,q,eps) :
 	"""Noisy-Max Mechanism 
-	noisyMax ( data , quality function , privacy parameter )
+	noisyMax ( data , domain, quality function , privacy parameter )
 	"""
-	# adds Laplace noise in scale - 1/eps to all the elements in X
-	noisy = [q(X,i)+np.random.laplace(0, 1/eps, 1) for i in R]
-	# returns the index of the maximal element from the noisy elements
+	# compute q(X,i) + Lap(1/eps) for all the elements in D
+	noisy = [q(X,i)+np.random.laplace(0, 1/eps, 1) for i in D]
+	# return element with maximum noisy q(X,i)
 	return 	noisy.index(max(noisy))
 
-def exponential_mechanism(X,R,q,eps) :
+def exponential_mechanism(X,D,q,eps) :
 	"""Exponential Mechanism 
-	expo ( data , range , quality function , privacy parameter )
+	expo ( data , domain , quality function , privacy parameter )
 	"""
-	# calculate a list of probabilities for each element in X (the)
-	R_chances = [math.exp(eps*q(X,r)) for r in R] 
-	normalizator = sum(R_chances) 
-	R_chances = [r/normalizator for r in R_chances]
-	# accumulates elements to get the CDF of the exponential distribution
-	R_CDF = np.cumsum(R_chances).tolist()
+	# calculate a list of probabilities for each element in the domain D
+	# probability of element d in D proportional to exp(eps*q(X,d)/2)
+	D_probability = [math.exp(eps*q(X,d)/2) for d in D] 
+	normalizer = sum(D_probability) 
+	D_probabiliry = [d/normalizer for d in D_chances]
+	# accumulate elements to get the CDF of the exponential distribution
+	D_CDF = np.cumsum(D_chances).tolist()
 	# use the uniform distribution (from 0 to 1) to pick an elements by the CDF
 	pick = np.random.rand()
-	return np.searchsorted(R_CDF,pick) + 1
+	return np.searchsorted(D_CDF,pick) + 1
 
