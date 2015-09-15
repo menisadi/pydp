39da991e8cbd67b10ac950154fd80eb235fff3ce
kobbi nissim <kobbini@hotmail.com>
2015-09-14 21:04:26 -0400

    added basis for recconcave

--- src/recconcave.py
+++ src/recconcave.py
@@ -2,12 +2,15 @@ import basicdp
 import numpy as np
 import math
 
-def RecBasis(T,q,r,alpha,eps,delta,S,N)
+def reconcave_basis(T,q,eps,S)
+	"""recursion basis for the reconcave procedure - execute the exponential mechanism
+    reconcave_basis(solution set size, quality function of sensitivity 1, eps privacy parameter, solution set)
+    """
 	return basicdp.expo(S, range(T+1), q, eps)
 
 def evaluate(T,q,r,alpha,eps,delta,S,N) :
 	if N == 1 or T <= 32 : 
-		return RecBasis(T,q,r,alpha,eps,delta,S,1)
+		return reconcave_basis(T,q,r,alpha,eps,delta,S)
 	else :
 		N = N - 1
 
