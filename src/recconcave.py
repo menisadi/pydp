a9eac99912fdbcb336758aa3693fb5c5602cbc7a
menisadi <menitm@gmail.com>
2015-09-17 15:27:47 +0300

    few fixes in code style

--- src/recconcave.py
+++ src/recconcave.py
@@ -1,28 +1,27 @@
 import basicdp
-import numpy as np
 import math
 
-def reconcave_basis(T,q,eps,S)
-	"""recursion basis for the reconcave procedure - execute the exponential mechanism
-    reconcave_basis(solution set size, quality function of sensitivity 1, eps privacy parameter, solution set)
-    """
-	return basicdp.expo(S, range(T+1), q, eps)
 
-def evaluate(T,q,r,alpha,eps,delta,S,N) :
-	if N == 1 or T <= 32 : 
-		return reconcave_basis(T,q,r,alpha,eps,delta,S)
-	else :
-		N = N - 1
-
-	logT = math.ceil(math.log(T,2))
-	Tup = 2**logT
-	def q2(X,r) : 
-		if T < r <= Tup : min(0,q(X,T))
-		else : return q(X,r)
-
-	
+def reconcave_basis(T, q, eps, S):
+    """recursion basis for the reconcave procedure - execute the exponential mechanism
+	reconcave_basis(solution set size, quality function of sensitivity 1, eps privacy parameter, solution set)
+	"""
+    return basicdp.exponential_mechanism(S, range(T + 1), q, eps)
 
 
-	
-	
+def evaluate(T, q, r, alpha, eps, delta, S, N):
+    if N == 1 or T <= 32:
+        return reconcave_basis(T, q, r, alpha, eps, delta, S)
+    else:
+        N -= 1
 
+    logT = math.ceil(math.log(T, 2))
+    Tup = 2 ** logT
+
+    def q2(X, r):
+        if T < r <= Tup:
+            min(0, q(X, T))
+        else:
+            return q(X, r)
+
+    return
