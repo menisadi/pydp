2228ddb002e4b96d73a48a880a065a5ae083cd5e
menisadi <menitm@gmail.com>
2015-09-24 14:12:40 +0300

    started step 3 in recconcave

--- src/recconcave.py
+++ src/recconcave.py
@@ -10,21 +10,29 @@ def reconcave_basis(T, q, eps, S):
     return basicdp.exponential_mechanism(S, range(T + 1), q, eps)
 
 
-def evaluate(T, q, r, alpha, eps, delta, S, N):
-    if N == 1 or T <= 32:
-        return reconcave_basis(T, q, r, alpha, eps, delta, S)
+def evaluate(range_max_value, quality_function, quality_promise, alpha, eps, delta, data, recursion_bound):
+    if recursion_bound == 1 or range_max_value <= 32:
+        return reconcave_basis(range_max_value, quality_function, quality_promise, alpha, eps, delta, data)
     else:
-        N -= 1
+        recursion_bound -= 1
 
-    logT = math.ceil(math.log(T, 2))
-    Tup = 2 ** logT
+    log_of_range = math.ceil(math.log(range_max_value, 2))
+    range_max_value_tag = 2 ** log_of_range
 
-    # TODO change the variables names to something more readable
-    def q_new(s, i):
-        if T < i <= Tup:
-            min(0, q(s, T))
+    def recursive_quality_function(data_base, i):
+        if range_max_value < i <= range_max_value_tag:
+            min(0, quality_function(data_base, range_max_value))
         else:
-            return q(s, i)
+            return quality_function(data_base, i)
+
+    # TODO finish this half beaked function
+    def intervals_quality(data_base, j):
+        intervals_sized_2_to_the_j = [range(range_max_value_tag)[i:i+(2 ** j)] for i in range(range_max_value-(2 ** j)+2)]
+        qualified_intervals = [[recursive_quality_function(data,i) for i in inner_list]
+                               for inner_list in intervals_sized_2_to_the_j]
+        # min_quality_in_intervals = lambda d i : [recursive_quality_function(data_base,i) for i in ]
+        # max(map(min,qualified_intervals))
+        return
 
     # def L(s, j):
     #     if 0 <= j <= logT:
