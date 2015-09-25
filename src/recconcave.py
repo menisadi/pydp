e5c1c6b225aa3b94cefd684077c681789c4596aa
menisadi <menitm@gmail.com>
2015-09-25 12:22:28 +0300

    recconcave - only step 9 left

--- src/recconcave.py
+++ src/recconcave.py
@@ -11,8 +11,10 @@ def reconcave_basis(T, q, eps, S):
 
 
 def evaluate(range_max_value, quality_function, quality_promise, approximation, eps, delta, data, recursion_bound):
+    # TODO go through variables names and see if they are more or less accurate
+    # TODO and maybe change some of the 'k' 'j' 'i'
     if recursion_bound == 1 or range_max_value <= 32:
-        return reconcave_basis(range_max_value, quality_function, quality_promise, approximation, eps, delta, data)
+        return reconcave_basis(range_max_value, quality_function, eps, data)
     else:
         recursion_bound -= 1
 
@@ -20,26 +22,27 @@ def evaluate(range_max_value, quality_function, quality_promise, approximation, 
     log_of_range = math.ceil(math.log(range_max_value, 2))
     range_max_value_tag = 2 ** log_of_range
 
-    def extended_quality_function(data_base, i):
-        if range_max_value < i <= range_max_value_tag:
+    def extended_quality_function(data_base, j):
+        if range_max_value < j <= range_max_value_tag:
             min(0, quality_function(data_base, range_max_value))
         else:
-            return quality_function(data_base, i)
+            return quality_function(data_base, j)
 
     # step 3
-    def intervals_quality(data_base, j):
+    def intervals_bounding(data_base, j):
         if j == log_of_range + 1:
-            return min(0, intervals_quality(data_base, j-1))
-        intervals_sized_2_to_the_j = [range(range_max_value_tag)[i:i+(2 ** j)]
-                                      for i in range(range_max_value-(2 ** j)+2)]
-        qualified_intervals = [[extended_quality_function(data_base, i) for i in inner_list]
-                                   for inner_list in intervals_sized_2_to_the_j]
+            return min(0, intervals_bounding(data_base, j-1))
+        # TODO should we add 1 to range input? (here and in step 7)
+        intervals_sized_2_to_the_j = [range(range_max_value_tag)[k:k+(2 ** j)]
+                                      for k in range(range_max_value-(2 ** j)+2)]
+        qualified_intervals = [[extended_quality_function(data_base, k) for k in inner_list]
+                               for inner_list in intervals_sized_2_to_the_j]
         return max(map(min, qualified_intervals))
 
     # step 4
     def recursive_quality_function(data_base, j):
-        return min(intervals_quality(data_base, j) - (1 - approximation) * quality_promise,
-                   quality_promise-intervals_quality(data_base, j + 1))
+        return min(intervals_bounding(data_base, j) - (1 - approximation) * quality_promise,
+                   quality_promise-intervals_bounding(data_base, j + 1))
 
     # step 5
     recursive_quality_promise = quality_promise * approximation / 2
@@ -47,4 +50,22 @@ def evaluate(range_max_value, quality_function, quality_promise, approximation, 
     # step 6 - recursion call
     recursion_returned = evaluate(log_of_range, recursive_quality_function, recursive_quality_promise, 1/4,
                                   eps, delta, data, recursion_bound)
-    return
+    good_interval = 8 * (2 ** recursion_returned)
+
+    # step 7
+    first_intervals = [range(range_max_value_tag)[i:i + good_interval]
+                       for i in range(0, range_max_value_tag, good_interval)]
+    second_intervals = [range(good_interval/2, range_max_value_tag)[i:i + good_interval]
+                        for i in range(0, range_max_value_tag-good_interval/2, good_interval)]
+
+    # step 8
+    def interval_quality(data_base, interval):
+        return max([extended_quality_function(data_base, j) for j in interval])
+
+    # TODO step 9 ('dist' algorithm needed)
+    first_chosen_interval = []
+    second_chosen_interval = []
+
+    # step 10
+    return basicdp.exponential_mechanism(data, first_chosen_interval.append(second_chosen_interval),
+                                         extended_quality_function, eps)
