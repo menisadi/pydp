1eb8c357487130e730a0caf6a6d39d833a3b7438
menisadi <menitm@gmail.com>
2015-11-27 13:14:50 +0200

    removed some of the printing

--- src/flat_concave.py
+++ src/flat_concave.py
@@ -7,7 +7,7 @@ def evaluate(data, range_max_value, quality_function, quality_promise, approxima
     # TODO go through variables names and see if they are more or less accurate
     # TODO and maybe change some of the 'k' 'j' 'i'
     # step 2
-    print "step 2"
+    # print "step 2"
     log_of_range = int(math.ceil(math.log(range_max_value, 2)))
     range_max_value_tag = 2 ** log_of_range
 
@@ -18,26 +18,26 @@ def evaluate(data, range_max_value, quality_function, quality_promise, approxima
             return quality_function(data_base, j)
 
     # step 4
-    print "step 4"
+    # print "step 4"
 
     def recursive_quality_function(data_base, j):
         return min(intervals_bounding(data_base, range_max_value_tag, j) - (1 - approximation) * quality_promise,
                    quality_promise-intervals_bounding(data_base, range_max_value_tag, j + 1))
 
     # step 6
-    print "step 6"
+    # print "step 6"
     recursion_returned = basicdp.exponential_mechanism(data, range(log_of_range+1), recursive_quality_function, eps)
 
     good_interval = 8 * (2 ** recursion_returned)
-    print "good interval: %d" % good_interval
+    # print "good interval: %d" % good_interval
 
     # step 7
-    print "step 7"
+    # print "step 7"
     first_intervals = [(i, i + good_interval) for i in xrange(0, range_max_value_tag, good_interval)]
     second_intervals = [(i, i + good_interval) for i in range(good_interval/2, range_max_value_tag, good_interval)]
 
     # step 9 ( using 'dist' algorithm)
-    print "step 9"
+    # print "step 9"
     first_chosen_interval = basicdp.a_dist(data, first_intervals, max_in_interval, eps, delta)
     second_chosen_interval = basicdp.a_dist(data, second_intervals, max_in_interval, eps, delta)
 
@@ -47,7 +47,7 @@ def evaluate(data, range_max_value, quality_function, quality_promise, approxima
         raise ValueError("stability problem, try taking more samples!")
 
     # step 10
-    print "step 10"
+    # print "step 10"
     first_chosen_interval_as_list = range(first_chosen_interval[0], first_chosen_interval[1]+1)
     second_chosen_interval_as_list = range(second_chosen_interval[0], second_chosen_interval[1]+1)
     return basicdp.exponential_mechanism(data, first_chosen_interval_as_list + second_chosen_interval_as_list,
