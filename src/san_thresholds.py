7e18c22845b12d73c139a087b89c49fe273b55f8
menisadi <menitm@gmail.com>
2016-03-08 15:28:05 +0200

    just one more line

--- src/san_thresholds.py
+++ src/san_thresholds.py
@@ -3,6 +3,7 @@ from math import log, ceil
 from qualities import points_in_subset, point_count_intervals_bounding
 from basicdp import exponential_mechanism, choosing_mechanism
 from bounds import log_star
+from functools import partial
 
 
 def sanitize(samples, domain_range, alpha, beta, eps, delta):
@@ -18,7 +19,8 @@ def __rec_sanitize__(samples, domain_range, alpha, beta, eps, delta, san_data, c
     calls -= 1
 
     # step 2
-    noisy_points_in_range = points_in_subset(samples, domain_range) + laplace(0, 1/eps, 1)
+    samples_domain_points = points_in_subset(data = samples)
+    noisy_points_in_range = samples_domain_points(domain_range) + laplace(0, 1/eps, 1)
     sample_size = len(samples)
 
     # step 3
@@ -50,5 +52,12 @@ def __rec_sanitize__(samples, domain_range, alpha, beta, eps, delta, san_data, c
     z = 2 ** z_tag
 
     #step 9
+    if z == 0:
+        b = choosing_mechanism(samples, range(domain_range[0], domain_range[1] + 1), samples_domain_points,
+                               1, alpha/64., beta, eps, delta)
+        a = b
+    else:
+        pass
+
     return
 
