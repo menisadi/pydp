22ac7516e475afdf6789a7c9ba418aa2c5392014
menisadi <menitm@gmail.com>
2016-03-28 14:48:50 +0300

    fix on san_thresholds_test.py still has run-time problem

--- tests/san_thresholds_test.py
+++ tests/san_thresholds_test.py
@@ -1,18 +1,16 @@
 from __future__ import division
 from numpy.random import exponential, normal, uniform
 import time
-from numpy import ceil, log2, linspace
+from numpy import ceil, log2, linspace, searchsorted
 import matplotlib.pyplot as plt
 from src.san_thresholds import sanitize
-
-
-def cdf(data_set, x):
-    return sum(1 for k in data_set if k < x)/len(data_set)
+from statsmodels.distributions.empirical_distribution import ECDF
 
 
 def plot_cdf(data_set, show=False):
-    xs = linspace(0, end_domain, 100)
-    ys = [cdf(data_set, j) for j in xs]
+    cf = ECDF(data_set)
+    xs = linspace(0, max(data_set), 100)
+    ys = [cf(j) for j in xs]
     plt.plot(xs, ys)
     if show:
         plt.show()
@@ -20,15 +18,15 @@ def plot_cdf(data_set, show=False):
 
 def cdf_comp(data1, data2):
     m = int(ceil(max(max(data1), max(data2))))
-    return 1-sum(1 for c in xrange(m) if abs(cdf(data1, c) - cdf(data2, c)) <= a)/m
+    f1 = ECDF(data1)
+    f2 = ECDF(data2)
+    return 1-sum(1 for c in xrange(m) if abs(f1(c) - f2(c)) <= a)/m
 
-
-start_time = time.time()
 
 a, b, e, d = 0.1, 0.1, 0.5, 2**-20
-samples_no = 1000
-parameter = 5
-data = [int(i) for i in exponential(parameter, samples_no)]
+samples_no = 10000
+parameter = 10
+data = [int(i) for i in normal(0, parameter, samples_no)]
 m = min(data)
 data = [i-m for i in data]
 print len(data)
@@ -37,9 +35,15 @@ print max_sample
 dim = ceil(log2(max_sample + 1))
 print dim
 end_domain = 2**int(dim)
-# plot_cdf(data, True)
+start_time = time.time()
 san = sanitize(data, (0, end_domain), a, b, e, d)
+run_time = time.time() - start_time
+print max(san)
 print cdf_comp(san, data)
 plot_cdf(data)
-plot_cdf(san)
+sorted_san = sorted(san)
+i_max_san = searchsorted(sorted_san, max_sample)
+plot_cdf(sorted_san[:i_max_san])
 plt.show()
+
+print "run-time: %.2f seconds" % run_time
