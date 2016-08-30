d06d4419243f676d3157be0d68ee689ab4ede237
menisadi <menitm@gmail.com>
2016-08-30 07:54:01 +0300

    documentation of good-radius

--- src/good_radius.py
+++ src/good_radius.py
@@ -7,26 +7,43 @@ from numpy.random import laplace
 
 
 def __max_average_ball__(radius, hood, t):
-    # TODO docstring
     """
-
-    :param radius:
-    :param hood:
-    :param t:
-    :return:
+    Quality function
+    Used in the procedure 'find' as the basis of the concave-quality-function
+    :param radius: possible radius to qualify
+    :param hood: distance matrix of all the points in the data. two-dimensional matrix.
+    :param t: number of desired points in the cluster (that 'find' is looking for)
+    :return: the maximum average number of points in t different balls of radius r,
+    when for balls with more than t points we take the value t (min(amount,t))
     """
     close_points = (hood <= radius).astype(int)
     a = sum(close_points, axis=1).argsort()[-t:]
     return sum(min(sum(i), t) for i in close_points[a]) / t
 
 
-def create_regular_domain(domain, dimension):
+def __create_regular_domain__(domain, dimension):
+    """
+    Helper function. Used as input for the Exponential Mechanism at the last step of the procedure 'find'
+    Given the domain properties and the dimension builds the domain of possible radius value
+    :param domain: tuple(absolute value of domain's end as int, minimum intervals in domain as float)
+    :param dimension: dimension of the vector space from which the data is drawn
+    :return:
+    """
     domain_end, domain_interval = domain
     domain_end *= 2*ceil(sqrt(dimension))
     return np.delete(arange(0, domain_end + domain_interval, domain_interval), 0)
 
 
-def sparse_domain(domain, dimension):
+def __sparse_domain__(domain, dimension):
+    """
+    Helper function. Used as input for the Exponential Mechanism at the last step of the procedure 'find'
+    Given the domain properties and the dimension builds a sparse version of the possible radius values
+    The list will not contain all the possible values but is build in a way which the Good-Radius
+    guarantees for the qualities of the resulting radius is maintained.
+    :param domain: tuple(absolute value of domain's end as int, minimum intervals in domain as float)
+    :param dimension: dimension of the vector space from which the data is drawn
+    :return:
+    """
     domain_end, domain_interval = domain
     domain_end *= 2*ceil(sqrt(dimension))
     new_domain = [domain_end]
@@ -37,16 +54,17 @@ def sparse_domain(domain, dimension):
 
 
 def find(data, domain, goal_number, failure, eps, sparse=True):
-    # TODO docstring
     """
-
-    :param data:
-    :param domain: (absolute value of domain's end as int, minimum intervals in domain as float)
-    :param goal_number:
-    :param failure:
-    :param eps:
-    :param sparse:
-    :return:
+    Based on "Locating a Small Cluster Privately" by Kobbi Nissim, Uri Stemmer, and Salil Vadhan. PODS 2016.
+    Given a data set, finds the radius of an approximately minimal cluster of points with
+    approximately the desired amount of points
+    :param data: list of points in R^dimension
+    :param domain: tuple(absolute value of domain's end as int, minimum intervals in domain as float)
+    :param goal_number: the number of desired points in the resulting cluster
+    :param failure: 0 < float < 1. chances that the procedure will fail to return an answer
+    :param eps: float > 0. privacy parameter
+    :param sparse: 1 > float > 0. privacy parameter
+    :return: the radius of the resulting cluster
     """
     # max(abs(np.min(data)), np.max(data))
     all_distances = distances(data)
@@ -61,9 +79,9 @@ def find(data, domain, goal_number, failure, eps, sparse=True):
     dimension = data.shape[1]
     # TODO maybe a little less sparse?
     if sparse:
-        new_domain = sparse_domain(domain, dimension)
+        new_domain = __sparse_domain__(domain, dimension)
     else:
-        new_domain = create_regular_domain(domain, dimension)
+        new_domain = __create_regular_domain__(domain, dimension)
 
     def quality(d, r):
         return min(goal_number - __max_average_ball__(r / 2, all_distances, goal_number),
